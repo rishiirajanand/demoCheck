be34254a96f246ba158a6adbd27e07207347b620
rishiirajanand <rishirajanand5@gmail.com>
2024-06-22 14:38:59 +0530

    changes

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const app = express()
 app.use(cors());
 
 app.use('/api', router)
-
+app.get('/', ()=>{
+    resizeBy.send('This is / routes page');
+})
 app.listen(3100, () => console.log('Server is running'))
 

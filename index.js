63d31a75c15e8dab7bd3c8bec27d257ab3fc22d3
rishiirajanand <rishirajanand5@gmail.com>
2024-06-22 14:43:29 +0530

    port changes

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 import express from "express";
 import cors from 'cors'
 import router from "./routes/route.js";
+import dotenv from 'dotenv'
+
+dotenv.config();
+
+const PORT = process.env.PORT || 1000;
+
 
 const app = express()
 app.use(cors());
 
 app.use('/api', router)
-app.get('/', ()=>{
-    resizeBy.send('This is / routes page');
+app.get('/', (req,res)=>{
+    res.send('This is / routes page');
 })
-app.listen(3100, () => console.log('Server is running'))
+app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`))
 
